3258cf5b4eaef9e984321195ae290fbea69bf7d9
James Correa <james.correa9090@gmail.com>
2021-11-02 14:50:35 -1000

    commit tree 4222629f121b591a2f32ab5dab4a58d1d84983b4

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+function receivesAFunction(receives){
+    return receives()
+  }
+  
+  
+function returnsANamedFunction(){
+    return function named(){
+    }
+  }
+  
+  
+function returnsAnAnonymousFunction(){
+    return function(){
+    }
+  }
